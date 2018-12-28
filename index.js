665f9eb2a7fa016511cf75c347131d99621d1f05
Xiaoyue Chen <38638844+Yvonne617@users.noreply.github.com>
2018-12-28 10:05:22 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const svg = d3.select('svg');
+d3.json('planets.json').then(data => {
+    const circs = svg.selectAll('circle')
+        .data(data);
+    circs.attr('cy',d=>d.cy)
+        .attr('cx',d=>d.distance)
+        .attr('r',d=>d.radius)
+        .attr('fill',d=>d.fill);
+    circs.enter()
+        .append('circle')
+        .attr('cy',d=>d.cy)
+        .attr('cx',d=>d.distance)
+        .attr('r',d=>d.radius)
+        .attr('fill',d=>d.fill);
+})
+
